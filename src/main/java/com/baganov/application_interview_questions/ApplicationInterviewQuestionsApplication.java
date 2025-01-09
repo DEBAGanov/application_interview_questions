2c422ef7eeb87a4ce755f631fca761e1d74551f1
Vladimir Baganov <baganov.v@gmail.com>
2025-01-09 23:14:28 +0300

    init

---
+++ src/main/java/com/baganov/application_interview_questions/ApplicationInterviewQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.baganov.application_interview_questions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationInterviewQuestionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationInterviewQuestionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baganov/application_interview_questions/ApplicationInterviewQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baganov.application_interview_questions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationInterviewQuestionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
